83684a30f3532afc8a36a7ab0562af4af7d31225
Vaishaliydv27 <Vaishaliydv27@users.noreply.github.com>
2024-08-11 15:37:40 +0000

    deploy: ea62761eb4abc04764145dc879117a3ef11e420d

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/react-chat-pwa/sw.js', { scope: '/react-chat-pwa/' })})}
